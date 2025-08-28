152599bf1d660392b2f42d78b39e12d0b94ed6b8
Shristi Khadka <123418291+shristikhadka@users.noreply.github.com>
2025-08-28 14:01:48 +0000

    factorial

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ exports.fibonacci = function(n) {
         return n;
     return this.fibonacci(n-1) + this.fibonacci(n-2);
 }
+
+exports.factorial = function(n) {
+    if (n < 0) {
+        return undefined; // new feature: no factorial for negatives
+    }
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    let result = 1;
+    for (let i = 2; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
